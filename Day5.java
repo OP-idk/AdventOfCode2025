bcec4a27177ebc7f7203ee1a1218599f2ebad970
OP-idk <ojparsley@gmail.com>
2025-12-10 22:43:53 -0800

    Progress on Day 5 Part 2
    I think I need to do some more planning on how to handle cases where a
    new range overlaps multiple existing ranges. Might try sorting? idk.

--- Day5.java
+++ Day5.java
@@ -45,4 +45,61 @@ public class Day5 {
 
         return numFresh;
     }
+
+    public static int part2() {
+        ArrayList<Long[]> ranges = new ArrayList<>();
+        try (Scanner s = new Scanner(new File("input5_ranges"))) {
+            while (s.hasNext()) {
+                String[] rangeStrings = s.nextLine().split("-");
+                Long[] range = new Long[2];
+                range[0] = Long.parseLong(rangeStrings[0]);
+                range[1] = Long.parseLong(rangeStrings[1]);
+                ranges.add(range);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+            return -1;
+        }
+
+        ArrayList<Long[]> condensedRanges = new ArrayList<>();
+        condensedRanges.add(ranges.get(0));
+        for (Long[] range :  ranges) {
+            boolean outside = true;
+            for (Long[] cRange : condensedRanges) {
+                if (range[0] > cRange[1] || range[1] < cRange[0]) {
+                    // If new range is completely outside current range, check the next one
+                    continue; 
+                }
+
+                if (range[0] < cRange[0]) {
+                    cRange[0] = range[0];
+                }
+                if (range[1] > cRange[1]) {
+                    cRange[1] = range[1];
+                }
+                outside = false;
+                break;
+                
+                /*
+                if (range[0] >= cRange[0]) { // Entirely enclosed or extending to right
+                    if (range[1] > cRange[1]) { // Extending to right
+                        cRange[1] = range[1];
+                    } 
+                    outside = false; 
+                    break; 
+                } else {
+                    cRange[0] = range[0];
+                    if (range[1] > cRange[1]) {
+                        cRange[1] = range[1];
+                    }
+                }
+                 */
+            }
+            if (outside) {
+                condensedRanges.add(range);
+            }
+        }
+
+        return -1;
+    }
 }
